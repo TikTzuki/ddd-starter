fb369639ae98f25a6ff44fd8d46cc02343c2d255
tiktzuki <tranphanthanhlong18@gmail.com>
2023-02-27 01:29:22 -0500

    first commit

---
+++ src/test/java/org/tiktzuki/dddstater/DddStaterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tiktzuki.dddstater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
